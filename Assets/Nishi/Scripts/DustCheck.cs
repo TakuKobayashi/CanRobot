04ca23f55724e879e2649e9b960b9c6f95da2b2e
nishi <icemaunntenn@gmail.com>
2016-02-20 12:20:03 +0900

    ドラック＆ドロップの処理

---
+++ Assets/Nishi/Scripts/DustCheck.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DustCheck : MonoBehaviour
+{
+    public bool[] m_Table;
+    public int test;
+
+    public void Awake()
+    {
+        m_Table[0] = test > 0;
+    }
+}
