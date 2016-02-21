28326540270e3c810704d86e5744e971f4b8c00d
nishi <icemaunntenn@gmail.com>
2016-02-21 14:01:41 +0900

    ピックアップメソッド追加

---
+++ Assets/Nishi/Scripts/PickupMethod.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PickupMethod : AbstractMethodBlock {
+
+    private Animator m_animator;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Method()
+    {
+        m_animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        if(true)
+        {
+            Method();
+        }
+        else
+        {
+            m_NextMethod.Method();
+        }
+    }
+}
