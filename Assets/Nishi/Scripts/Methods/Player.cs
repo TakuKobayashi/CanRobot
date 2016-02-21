d09ace6cbc9fcc718529de8278c80eecab225335
ReiKalan <reika_kalanchoe@live.jp>
2016-02-21 12:34:46 +0900

    UI追加　Scene管理ついか　

---
+++ Assets/Nishi/Scripts/Methods/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace CanRobot.Methods
+{
+    public class Player : MonoBehaviour
+    {
+        /// <summary>
+        /// 動く
+        /// </summary>
+        public void Move()
+        {
+            //アニメーション切り替え
+        }
+        /// <summary>
+        /// 右向く
+        /// </summary>
+        public void RightTurn()
+        {
+            //アニメーション切り替え
+        }
+
+        /// <summary>
+        /// 左向く
+        /// </summary>
+        public void LeftTurn()
+        {
+            //アニメーション切り替え
+        }
+
+        /// <summary>
+        /// 拾う
+        /// </summary>
+        public void PickUp()
+        {
+            //アニメーション切り替え
+        }
+
+    }
+}
---
+++ Assets/Nishi/Scripts/SceneManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+/// <summary>
+/// シーンマネージャー
+/// </summary>
+public class SceneManager : MonoBehaviour {
+    //シーンのロード
+    public void TitleLoad() { UnityEngine.SceneManagement.SceneManager.LoadScene("Title", LoadSceneMode.Additive); }
+    public void MainLoad() { UnityEngine.SceneManagement.SceneManager.LoadScene("Main", LoadSceneMode.Additive); }
+    public void GameClearLoad() { UnityEngine.SceneManagement.SceneManager.LoadScene("GameClear", LoadSceneMode.Additive); }
+    public void GameOverLoad() { UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver", LoadSceneMode.Additive); }
+
+    //シーンのアンロード
+    public void TitleUnLoad() { UnityEngine.SceneManagement.SceneManager.UnloadScene("Title"); }
+    public void MainUnLoad() { UnityEngine.SceneManagement.SceneManager.UnloadScene("Main"); }
+    public void GameClearUnLoad() { UnityEngine.SceneManagement.SceneManager.UnloadScene("GameClear"); }
+    public void GameOverUnLoad() { UnityEngine.SceneManagement.SceneManager.UnloadScene("GameOver"); }
+
+}
