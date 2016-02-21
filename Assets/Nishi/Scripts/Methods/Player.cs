903caafe82ea27b47dab61bde0ad3d8a041c1f60
ReiKalan <reika_kalanchoe@live.jp>
2016-02-21 14:44:49 +0900

    SE tuika

--- Assets/Nishi/Scripts/Methods/Player.cs
+++ Assets/Nishi/Scripts/Methods/Player.cs
@@ -20,47 +20,18 @@ namespace CanRobot.Methods
         {
             _AudioSource = this.GetComponent<AudioSource>();
         }
-        public void Update()
-        {
-            SEUpdate();
-        }
-
-        /// <summary>
-        /// サウンドエフェクトのアップデート
-        /// </summary>
-        private void SEUpdate()
-        {
-            if (_RobotState == RobotState.Idle)
-            {
-                _AudioSource.loop = false;
-                _AudioSource.clip = Resources.Load("Sounds/SE/Idle") as AudioClip;
-            }
-            else if
-                (_RobotState == RobotState.Move)
-            {
-                _AudioSource.loop = false;
-                _AudioSource.clip = Resources.Load("Sounds/SE/Move") as AudioClip;
-            }
-            else if (_RobotState == RobotState.Turn)
-            {
-                _AudioSource.loop = false;
-                _AudioSource.clip = Resources.Load("Sounds/SE/Turn") as AudioClip;
-            }
-            else if (_RobotState == RobotState.PickUp)
-            {
-                _AudioSource.loop = false;
-                _AudioSource.clip = Resources.Load("Sounds/SE/PickUp") as AudioClip;
-            }
-        }
 
         /// <summary>
         /// 動く アニメーション切り替え
         /// </summary>
         public void Idle()
         {
+            _AudioSource.loop = false;
+            //_AudioSource.clip = Resources.Load("Sounds/SE/SE_Idle_000") as AudioClip;
+
             _AudioSource.Stop();
             _RobotState = RobotState.Idle;
-            //アニメーション切り替え
+            this.GetComponent<Animator>().SetInteger("State", (int)_RobotState);
         }
 
         /// <summary>
@@ -68,6 +39,9 @@ namespace CanRobot.Methods
         /// </summary>
         public void Move()
         {
+            _AudioSource.loop = true;
+            _AudioSource.clip = Resources.Load("Sounds/SE/SE_Move_000") as AudioClip;
+
             _AudioSource.Play();
             _RobotState = RobotState.Move;
             this.GetComponent<Animator>().SetInteger("State",(int)_RobotState);
@@ -78,6 +52,9 @@ namespace CanRobot.Methods
         /// </summary>
         public void RightTurn()
         {
+            _AudioSource.loop = false;
+            _AudioSource.clip = Resources.Load("Sounds/SE/SE_Turn_000") as AudioClip;
+
             _AudioSource.Play();
             _RobotState = RobotState.Turn;
             this.GetComponent<Animator>().SetInteger("State", (int)_RobotState);
@@ -88,6 +65,9 @@ namespace CanRobot.Methods
         /// </summary>
         public void LeftTurn()
         {
+            _AudioSource.loop = false;
+            _AudioSource.clip = Resources.Load("Sounds/SE/SE_Turn_000") as AudioClip;
+
             _AudioSource.Play();
             _RobotState = RobotState.Turn;
             this.GetComponent<Animator>().SetInteger("State", (int)_RobotState);
@@ -98,10 +78,12 @@ namespace CanRobot.Methods
         /// </summary>
         public void PickUp()
         {
+            _AudioSource.loop = false;
+            _AudioSource.clip = Resources.Load("Sounds/SE/SE_PickUp_000") as AudioClip;
+
             _AudioSource.Play();
             _RobotState = RobotState.PickUp;
             this.GetComponent<Animator>().SetInteger("State", (int)_RobotState);
-
         }
 
     }
